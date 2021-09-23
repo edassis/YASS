ccb6362593db936b18145ffddd35f20bb082a8a7
Eduardo Assis <eduardoffassis@gmail.com>
2021-09-23 09:59:01 -0300

    Adds fsanitize into cmake

--- src/application/Main.cpp
+++ src/application/Main.cpp
@@ -1,11 +1,7 @@
 #include "engine/Game.h"
-#include "engine/GameObject.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-
-#include "engine/TileMap.h"
-#include "engine/TileSet.h"
 
 #define VNAME(x) #x
 #define VDUMP(x) std::cout << #x << " " << x << std::endl
