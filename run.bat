bf42ab455c1b4c33b7d20dff125dbb991002b0d3
Eduardo Assis <eduardoffassis@gmail.com>
2022-03-09 20:32:32 -0300

    Alternatives ways to build (Windows)

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 @echo off
 
+@REM cmake -G "Unix Makefiles" -B build -Wno-dev
 cmake -G "Unix Makefiles" -B build -DCMAKE_BUILD_TYPE=Debug -Wno-dev
 
+@REM cmake --build build --config Debug --target all
 cmake --build build
