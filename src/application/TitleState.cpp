241ee8939fc1d931db237960031409817503b83f
Eduardo Assis <eduardoffassis@gmail.com>
2022-03-24 19:05:45 -0300

    TitleState simple text to start game

--- src/application/TitleState.cpp
+++ src/application/TitleState.cpp
@@ -5,12 +5,22 @@
 #include "engine/GameObject.h"
 #include "engine/InputManager.h"
 #include "engine/CameraFollower.h"
+#include "engine/Text.h"
 
 TitleState::TitleState() {
     auto* titleGO = new GameObject();
     titleGO->AddComponent(*new CameraFollower(*titleGO));
     titleGO->AddComponent(*new Sprite(*titleGO, "assets/img/title.jpg"));
     AddObject(*titleGO);
+
+    auto* textGO = new GameObject();
+    std::string textString = "Aperte \"Espaco\" para continuar";
+    SDL_Color textColor = {0, 0, 0, 0xff};
+    auto* textComponent = new Text(*textGO, "assets/font/Call me maybe.ttf", 14, Text::BLENDED, textString, textColor);
+    textGO->AddComponent(*textComponent);
+    AddObject(*textGO);
+    
+    textGO->box.Centralize(mat::Vec2{480.f, 500.f});
 }
 
 void TitleState::Start() {
